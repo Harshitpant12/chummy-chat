74fec22d8708188eef0d1c487c7927aa0d3d85e9
Harshitpant12 <harshpant778@gmail.com>
2026-02-08 14:56:27 +0530

    Implement logout, profile update, auth check, and cloudinary setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs"
 
 import User from "../models/user.model.js"
 import { generateToken } from "../lib/utils.js"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -75,10 +76,44 @@ export const login = async (req, res) => {
     } catch (error) {
         console.log("Error in login controller", error.message);
         res.status(500).json({ message: "Internal Server Error" })
-
     }
 }
 
 export const logout = (req, res) => {
-    res.send("logout route")
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logged out successfully" })
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profillePic } = req.body;
+        const userId = req.user._id // we're calling next function in protectRoute and there we've defined about user that's why here we can use user directly
+
+        if (!profillePic) {
+            return res.status(400).json({ message: "Profile pic is required" })
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profillePic) // uploading in cloudinary, cloudinary is just a bucket for our images
+        const updatedUser = await User.findById(userId, { profilePic: uploadResponse.secure_url }, { new: true }) // hover over new to get more details
+
+        res.status(200).json(updatedUser) // so that ui can be updated immediately
+    } catch (error) {
+        console.log("Error in update profile", error.message);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json()) // allow to extract json data out of body
+app.use(cookieParser()) // allow to parse the cookies
 
 app.use("/api/auth", authRoutes)
 
