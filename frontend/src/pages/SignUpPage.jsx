1a0e96217daac7dfbe14a833e216b1c8f5fd15ff
Harshitpant12 <harshpant778@gmail.com>
2026-02-09 13:25:01 +0530

    set up project structure and initial signup page

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const SignUpPage = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+    })
+
+    return (
+        <div>
+        SignUp
+        </div>
+    )
+}
+
+export default SignUpPage
