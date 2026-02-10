5a66fdc5b586bb59056e518f7b32e2fb5b093e9b
Harshitpant12 <harshpant778@gmail.com>
2026-02-10 16:31:16 +0530

    restructure project and update environment configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path";
 
 import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
@@ -11,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config()
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "10mb" })); // to increase data limit specially for image
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -25,6 +27,14 @@ app.use(cors(
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 const startServer = async () => {
     try {
